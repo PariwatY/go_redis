3a5971bf1a3311e78b35a7c3ed6ac20c0cb92fb4
Pariwat Yimpayoon <32978747+PariwatY@users.noreply.github.com>
2022-05-27 22:22:40 +0700

    add redis

--- main.go
+++ main.go
@@ -1,19 +1,39 @@
 package main
 
 import (
-	"time"
+	"fmt"
+	"go_redis/repositories"
 
-	"github.com/gofiber/fiber/v2"
+	"github.com/go-redis/redis/v8"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 )
 
 func main() {
-	app := fiber.New()
+	db := initialDB()
+	redisClient := initRedis()
 
-	app.Get("hello", func(c *fiber.Ctx) error {
-		time.Sleep(time.Millisecond * 10)
-		return c.SendString("hello")
+	productRepo := repositories.NewProductRepositoryRedis(db, redisClient)
+
+	products, err := productRepo.GetProducts()
+	if err != nil {
+		fmt.Print(err)
+	}
+
+	fmt.Print(products)
+}
+
+func initialDB() *gorm.DB {
+	dsn := "root:password@tcp(127.0.0.1:3306)/go_redis?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+	return db
+}
+
+func initRedis() *redis.Client {
+	return redis.NewClient(&redis.Options{
+		Addr: "localhost:6379",
 	})
-
-	app.Listen(":8000")
-
 }
